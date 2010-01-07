cfd7949b6da8f280969e23d69a7ab07ccdc80ad5
Samuel Defago <defagos@gmail.com>
2010-01-07 19:17:38 +0100

    New IDE path shortcuts; fixed existing ones

--- devtools/launchVisualStudio.bat
+++ devtools/launchVisualStudio.bat
@@ -11,19 +11,28 @@ if not "%2"=="" goto Usage
 rem Check that the environment has been generated
 if not exist %1 goto NotExist
 
-set ROOT_DIR=..
+set ROOT_DIR=%cd%\..
 
 rem Define some useful shortcuts for use in the IDE
 rem -----------------------------------------------
 
 rem Include directories
-set i=%ROOT_DIR%include\
+set i=%ROOT_DIR%\include
+set ic=%i%\core
+set is=%i%\synfonia
+set iu=%i%\utils
 
 rem Source directories
-set s=%ROOT_DIR%src\
+set s=%ROOT_DIR%\src
+set sc=%s%\core
+set ss=%s%\synfonia
+set su=%s%\utils
 
 rem Test directories
-set t=%ROOT_DIR%test\
+set t=%ROOT_DIR%\test
+set tc=%t%\core
+set ts=%t%\synfonia
+set tu=%t%\utils
 
 rem Launch the IDE
 rem --------------
